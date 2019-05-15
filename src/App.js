b1048588751594cbbb582aa0834e8002008ee004
Saurabh <saurabhsinghal98@outlook.com>
2019-05-15 23:37:43 +0530

    final commit -some essential comments added to project for better understanding

--- src/App.js
+++ src/App.js
@@ -16,43 +16,48 @@ class App extends Component {
 
 
 
-   componentDidMount(){
+  componentDidMount(){
     fetch("https://api.coingecko.com/api/v3/coins/list")
     .then(res => res.json())
     .then((result) => {
       this.setState({isLoad : true});
     })
   }
-
+//in componentDidMount,there is api call which fetch cryptocoin list and help in setting coin Loading
+//until searchbar appears
 
 
   fun = ()=>{
+    //this function take coin value from searchbar and after that we call respective coin data through given api
     let a = document.getElementsByTagName('input')[0].value;
     a = a.replace(/^\s+/g, '');
     if(a!==""){
-    a = a.toLowerCase();
-    fetch("https://api.coingecko.com/api/v3/coins/"+a)
-    .then(res => res.json())
-    .then((result) => {
-      this.setState({items : result,isLoadBody : true});
-    }
-  )
+      a = a.toLowerCase();
+      fetch("https://api.coingecko.com/api/v3/coins/"+a)
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({items : result,isLoadBody : true});
+        //result will store in items
+      }
+    )
   }
+}
+
+
+
+render() {
+  const {isLoad,items,isLoadBody} = this.state;
+  if(!isLoad){
+    //this will render until we load coin list
+    return(
+      <div>
+      <h1>Crypto Wiki</h1>
+      <h3>Loading The Coin List</h3>
+      </div>
+    )
   }
-
-
-
-  render() {
-    const {isLoad,items,isLoadBody} = this.state;
-    if(!isLoad){
-      return(
-        <div>
-        <h1>Crypto Wiki</h1>
-        <h3>Loading The Coin List</h3>
-        </div>
-      )
-    }
-    else{
+  else{
+    //after the loading of list of coins this will render and state(isLoad) helped to do it
     return (
       <div>
       <h1>Crypto Wiki</h1>
@@ -60,10 +65,13 @@ class App extends Component {
       <button onClick={this.fun}>Get Info About Coin</button><br/>
       <p>Search For a Coin</p>
       <Call description={items} loading={isLoadBody}/>
+      {// call component will help to display the coin information in the required information
+        //api result in the form of props we pass item and isLoadBody to it
+      }
       </div>
     );
 
   }
-  }
+}
 }
 export default App;
--- src/Call.js
+++ src/Call.js
@@ -2,21 +2,23 @@ import React,{Component} from 'react';
 class Call extends Component {
 
   render(){
-  if(this.props.description.error){
-    return(
-      <div>Not Found</div>
-    )
-  }
-  if(this.props.loading){
-  return(
-    <div className="desc" style={{display: 'block'}}>
-    <div className="desc-head">
-      <a className ="title" href={this.props.description.links.homepage[0]}>{this.props.description.name} - {this.props.description.symbol}</a>
-    </div>
+    if(this.props.description.error){
+      //if searched coin is not a coin then api will through an error so corresponding Not Found will render
+      return(
+        <div>Not Found</div>
+      )
+    }
+    if(this.props.loading){
+      //if searched coin is correct and corresponding api call return a valid result  and we render it in requrired format
+      return(
+        <div className="desc" style={{display: 'block'}}>
+        <div className="desc-head">
+        <a className ="title" href={this.props.description.links.homepage[0]}>{this.props.description.name} - {this.props.description.symbol}</a>
+        </div>
 
-    <div className="desc-body-left">
-      <div className="image"><img alt="Loading.." src={this.props.description.image.large}/>
-      <hr/>
+        <div className="desc-body-left">
+        <div className="image"><img alt="Loading.." src={this.props.description.image.large}/>
+        <hr/>
         Country of origin - {this.props.description.Country_origin}<br/>
         Date of Appearance - <br/>
         {this.props.description.genesis_date}<br/>
@@ -29,19 +31,22 @@ class Call extends Component {
         Community Score - {this.props.description.community_score}<br/>
         Liquidity Score - {this.props.description.liquidity_score}<br/>
         Public Interest Score - {this.props.description.public_interest_score}<br/>
-      </div>
+        </div>
+        {
+          //dangerouslySetInnerHTML is to render the text as html not as text which is actually requried
+        }
+        <div className="desc-body-right" dangerouslySetInnerHTML={{__html: this.props.description.description.en}}>
 
-    <div className="desc-body-right" dangerouslySetInnerHTML={{__html: this.props.description.description.en}}>
+        </div>
+        </div>
+        </div>
+      )
 
-    </div>
-    </div>
-    </div>
-  )
-
-}
-else{
-  return(<div></div>)
-}
-}
+    }
+    else{
+      //in start when no search query is passed then nothing should be render so empty div(simple,but can be another way to do it)
+      return(<div></div>)
+    }
+  }
 }
 export default Call;
